693ae0e046420e328c98248b36c9a59afcf23785
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-14 16:32:53 +0530

    Uc3

---
+++ Day30-TDDProblem/CabInvoiceException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CabInvoiceGenerator
+{
+    [Serializable]
+    internal class CabInvoiceException : Exception
+    {
+        private object iNVALID_USER_ID;
+        private string v;
+
+        public CabInvoiceException()
+        {
+        }
+
+        public CabInvoiceException(string message) : base(message)
+        {
+        }
+
+        public CabInvoiceException(object iNVALID_USER_ID, string v)
+        {
+            this.iNVALID_USER_ID = iNVALID_USER_ID;
+            this.v = v;
+        }
+
+        public CabInvoiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CabInvoiceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public static object ExceptionType { get; internal set; }
+    }
+}
